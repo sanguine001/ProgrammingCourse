559d1e2fca6febba0b165c62f64d2192064f5a6c
Business <Business@10.250.36.115>
2017-02-06 18:20:53 -0500

    assignment 2

---
+++ CIS3260/src/Car.java
@@ -0,0 +1,57 @@
+
+public class Car {
+	//attributes
+	int gear = 1;
+	int location;
+	int speed = 0;
+	final int destination = 250;
+	
+	//methods
+	int reportGear() {
+		return gear;
+	}
+	void reverseGear() {
+		gear = gear - 1 ;
+	}
+    void gearUp() {
+    	if(gear >= 6){
+    		System.out.println("Error: Gear cannot be increase.");
+    	}
+    	else { gear = gear+ 1;}
+    }
+	void gearDown() {
+		if(gear <= -1){
+			System.out.print("Error: Gear is in the reverse gear; Can not decrease any further");
+		}
+		else {gear = gear--; }
+	}
+	int reportLocation() {
+		return location;
+	}
+	int reportRemaining() {
+		return destination - location;
+		
+	}
+	void moveByTime(int x) {
+		if(x >=0){
+		// x = time
+			speed = gear * 20;
+			int delta = speed * x;
+			location = location + delta;
+		}
+		
+		else{
+			System.out.println("Error: Time is should be positive.");
+		}
+		
+	}
+	boolean isArrived() {
+		if (location >= destination ){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+}
